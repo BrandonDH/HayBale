0db0f0db5bc5c8b480e839f442a9afc8a6166506
Brandon Herford <brandonherford@gmail.com>
2019-05-04 22:23:26 -0400

    Update content and layout

--- process_email_groups.js
+++ process_email_groups.js
@@ -1,7 +1,7 @@
-let raw_email_data = 'raw_email_data.txt';
-let processed = 'processed_emails.txt';
 let listToProcess = document.getElementById('emailList');
 
 listToProcess.addEventListener('keyup',function() {
-document.getElementById('alert').innerHTML = "{" + this.value.split(" ").join(", ") + "}";
+  let csl = "{" + this.value.split(" ").join(", ") + "}";
+  let listItem = document.getElementById('alert');
+  listItem.innerHTML = csl;
 })
