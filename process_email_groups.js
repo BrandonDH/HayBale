925321992aa32eab9a07e43f7233f4d972493dfa
Brandon Herford <brandonherford@gmail.com>
2019-05-05 19:43:53 -0400

    Update logic and layout

--- process_email_groups.js
+++ process_email_groups.js
@@ -2,6 +2,8 @@ let listToProcess = document.getElementById('emailList');
 
 listToProcess.addEventListener('keyup',function() {
   let csl = "{" + this.value.split(" ").join(", ") + "}";
+
   let listItem = document.getElementById('alert');
+  
   listItem.innerHTML = csl;
 })
